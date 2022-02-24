ce667b23a5c9fb1d97df80546eca7a56d0082959
Ana Catarina Pinho-Gomes <anacatarina.pinhogomes@gmail.com>
2022-02-24 09:24:47 +0000

    analysis csv files

--- analysis/report.R
+++ analysis/report.R
@@ -41,6 +41,7 @@ df_input <- df_input[df_input$CVD_code==0 & df_input$CKD_code==0 & df_input$T1D_
 
 nrow(df_input)
 
+
 ## exclude patients with risk assessment more than 5 years ago
 
 summary(df_input$CVD_assess_latest_date)
@@ -51,9 +52,16 @@ summary(df_input_2$CVD_assess_latest_date)
 
 nrow(df_input)
 
+
 ## calculate % patients with CVD assessment in past 5 years
 
 summary(df_input_2$CVD_assess_latest_number)
+
+df_input_2$CVD_assess_cat <- ifelse(is.na(df_input_2$CVD_assess_latest_number), 0, 1)
+
+cvd.risk <- summary(as.factor(df_input_2$CVD_assess_cat))
+
+write.csv(cvd.risk, "cvd.risk.5years.csv")
 
 
 ## calculate % patients with CVD risk over 10 on statins (NA means not on statins)
@@ -62,6 +70,12 @@ data_risk10 <- df_input_2[df_input_2$CVD_assess_latest_number>=10,]
 
 summary(data_risk10$statins_prescribed)
 
+data_risk10$statin_cat <- ifelse(is.na(data_risk10$statins_prescribed), 0, 1)
+
+statin.risk10 <- summary(as.factor(data_risk10$statin_cat))
+
+write.csv(statin.risk10, "statin.risk10.csv")
+
 
 ## calculate % patients with CVD risk over 20 on statins (NA means not on statins)
 
@@ -69,15 +83,31 @@ data_risk20 <- df_input_2[df_input_2$CVD_assess_latest_number>=20,]
 
 summary(data_risk20$statins_prescribed)
 
+data_risk20$statin_cat <- ifelse(is.na(data_risk20$statins_prescribed), 0, 1)
+
+statin.risk20 <- summary(as.factor(data_risk20$statin_cat))
+
+write.csv(statin.risk20, "statin.risk20.csv")
+
 
 ## calculate % patients with pre-existing CVD on statins (NA means not on statins)
 
 summary(df_cvd$statins_prescribed)
+
+df_cvd$statin_cat <- ifelse(is.na(df_cvd$statins_prescribed), 0, 1)
+
+statin.cvd <- summary(as.factor(df_cvd$statin_cat))
+
+write.csv(statin.cvd, "statin.cvd.csv")
 
 
 ## calculate % patients with CKD on statins (NA means not on statins)
 
 summary(df_ckd$statins_prescribed)
 
+df_ckd$statin_cat <- ifelse(is.na(df_ckd$statins_prescribed), 0, 1)
 
+statin.ckd <- summary(as.factor(df_ckd$statin_cat))
+
+write.csv(statin.ckd, "statin.ckd.csv")
 
